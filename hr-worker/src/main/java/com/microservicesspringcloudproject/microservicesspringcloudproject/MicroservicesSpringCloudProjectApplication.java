804f7064f68cde74f2d988e2cc1190a279192f6f
João Lima <joaopedro.donatolima@hotmail.com>
2024-01-25 20:36:29 -0300

    hr-worker created

---
+++ hr-worker/src/main/java/com/microservicesspringcloudproject/microservicesspringcloudproject/MicroservicesSpringCloudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.microservicesspringcloudproject.microservicesspringcloudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringCloudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesSpringCloudProjectApplication.class, args);
+	}
+
+}
---
+++ hr-worker/src/test/java/com/microservicesspringcloudproject/microservicesspringcloudproject/MicroservicesSpringCloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesspringcloudproject.microservicesspringcloudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSpringCloudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
