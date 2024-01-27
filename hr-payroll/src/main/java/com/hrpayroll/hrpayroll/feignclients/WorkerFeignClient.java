cd6b40151673ae9ce3d81f8a205fa9e5f681848b
João Lima <joaopedro.donatolima@hotmail.com>
2024-01-26 23:04:01 -0300

    feignClient

---
+++ hr-payroll/src/main/java/com/hrpayroll/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,15 @@
+package com.hrpayroll.hrpayroll.feignclients;
+
+import com.hrpayroll.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+    @GetMapping("/{idWorker}")
+    ResponseEntity<Worker> findWorkerById(@PathVariable Long idWorker);
+}
