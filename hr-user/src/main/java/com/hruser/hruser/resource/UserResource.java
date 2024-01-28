3b717605c02548f8baf2732bdfa756ff6487ee79
João Lima <joaopedro.donatolima@hotmail.com>
2024-01-28 17:13:55 -0300

    UserRepository, UserResource and Zuul config

---
+++ hr-user/src/main/java/com/hruser/hruser/resource/UserResource.java
@@ -0,0 +1,25 @@
+package com.hruser.hruser.resource;
+
+import com.hruser.hruser.entities.User;
+import com.hruser.hruser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @Autowired private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        User user = userRepository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+}
