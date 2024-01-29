2640b0a3756a6a727e499f44ef000100388dc5a7
João Lima <joaopedro.donatolima@hotmail.com>
2024-01-28 22:02:57 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/hroauth/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/hroauth/hroauth/services/UserService.java
@@ -7,8 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -22,6 +26,17 @@ public class UserService {
             throw new IllegalArgumentException("Email not found");
         }
         logger.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+        logger.info("Email found: " + username);
         return user;
     }
 }
